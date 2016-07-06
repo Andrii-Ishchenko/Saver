6de747f0be89a4a21f6e2b3ef3569a1750669e79
Andrii-Ishchenko <andrey.ischencko@gmail.com>
2016-07-06 18:12:21 +0300

    link function in expense tracker directive

--- app/components/expenseTracker/expenseTrackerDirective.js
+++ app/components/expenseTracker/expenseTrackerDirective.js
@@ -3,15 +3,19 @@
 	
 	angular.module('saver').directive('expenseTrackerDirective',expenseTrackerDirective);
 	
-	console.log("expense tracker directive global func")
 	
 	function expenseTrackerDirective(){
 	
-		console.log("expense tracker dir");
+		function link(scope, element, attr){
+			var ddNode = $("#categoriesDropdown",element);
+			if(ddNode){
+				ddNode.dropdown();
+			}
+		}
 		
 		return {
 			restrict : 'E',
-			link: function(){console.log("exp track dir link");},
+			link: link,
 			replace: true,
 			templateUrl:'app/components/expenseTracker/expenseTracker.html'
 		}
